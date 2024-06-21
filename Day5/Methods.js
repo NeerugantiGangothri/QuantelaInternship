d2730428e3e400420051c1800c5c67b4361960a0
Neeruganti Gangothri <96761192+NeerugantiGangothri@users.noreply.github.com>
2024-06-21 19:07:14 +0530

    Add files via upload

---
+++ Day5/Methods.js
@@ -0,0 +1,12 @@
+class ClassEx {
+  publicMethod1() {
+    return "hello world";
+  }
+  PublicMethod2() {
+    return "Welcome";
+  }
+}
+
+const construct = new ClassEx();
+console.log(construct.publicMethod1()); // "hello world"
+console.log(construct.PublicMethod2());
