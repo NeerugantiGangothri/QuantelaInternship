3d90a8233dee6a6b37c5466252ccd24e3883cded
Neeruganti Gangothri <96761192+NeerugantiGangothri@users.noreply.github.com>
2024-07-15 19:12:55 +0530

    Add files via upload

---
+++ Day23_15_07_2024/asyncawain1.js
@@ -0,0 +1,46 @@
+const p1=new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve('success')
+        },2000)
+});
+const p2=new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve('success')
+        },5000)
+});
+
+async function handlePromise(){
+   
+    const res2=await p2;
+    console.log("Hii");
+    console.log(res2);
+
+    const res1=await p1;
+    console.log("Hii");
+    console.log(res1);
+
+}
+handlePromise();  // After 5 second all values will be printed
+
+async function handlePromise(){
+   
+
+    const res1=await p1;
+    console.log("Hii");
+    console.log(res1);
+
+    const res2=await p2;
+    console.log("Hii");
+    console.log(res2);
+
+}
+handlePromise();  // after 2 seconds p1 promise printed after 5 seconds p2 printed
+
+
+function getData(){
+    //Js engine will not wait promise to resolve
+    Hii , Success
+    p.then(res=>console.log(res));
+    console.log("Hii")
+}
+getData();
